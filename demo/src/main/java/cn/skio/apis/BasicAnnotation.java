5f1e961778f35cdca6d3840f0175be57856a07e1
范黎琳 <fanlilin@skio.cn>
2019-09-26 16:54:32 +0800

    第一次提交TestNG demo

---
+++ demo/src/main/java/cn/skio/apis/BasicAnnotation.java
@@ -0,0 +1,16 @@
+package cn.skio.apis;
+
+import org.testng.annotations.Test;
+
+/**
+ * @program: AutoApiTestDemo
+ * @description:
+ * @author: May
+ * @create: 2019-09-26 16:52
+ */
+public class BasicAnnotation {
+  @Test
+  public void test() {
+    System.out.println("这是一个测试用例");
+  }
+}
